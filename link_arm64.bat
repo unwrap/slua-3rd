748ee28813d7876627ff6e41c96884826e34f852
小河 <unwrap@me.com>
2020-01-06 18:21:22 +0800

    build_arm64

--- link_arm64.bat
+++ link_arm64.bat
@@ -1,9 +1,9 @@
 @echo off
 set ndkPath=D:/android-ndk-r15c
 cd ./platform-android
-call %ndkPath%/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a" APP_PLATFORM=android-21
-call %ndkPath%/ndk-build APP_ABI="arm64-v8a" APP_PLATFORM=android-21
+call %ndkPath%/build/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a" APP_PLATFORM=android-21
+call %ndkPath%/build/ndk-build APP_ABI="arm64-v8a" APP_PLATFORM=android-21
 copy libs\arm64-v8a\libslua.so ..\Plugins\Android\libs\arm64-v8a
-call %ndkPath%/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a" APP_PLATFORM=android-21
+call %ndkPath%/build/ndk-build clean APP_ABI="armeabi-v7a,x86,arm64-v8a" APP_PLATFORM=android-21
 echo Successfully linked
 exit
